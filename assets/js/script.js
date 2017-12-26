83e0ca7f670e19f5fc2da9af4df1783b630decd8
Daniel Mao <daniel.mao@berkeley.edu>
2017-12-25 19:22:23 -0800

    styling/lazy loading

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,75 +1,80 @@
 $(document).ready(function() {
  
-var offset = 500;
-var duration = 300;
- 
-$(window).scroll(function() {
- 
-  if ($(this).scrollTop() > offset) {
-    $(".back-to-top").fadeIn(duration);
-  } else {
-    $(".back-to-top").fadeOut(duration);
-  }
-});
- 
-$(".back-to-top").click(function(event) {
-  event.preventDefault();
-  $("html, body").animate({scrollTop: 0}, duration);  
-return false;
-});
+	var offset = 500;
+	var duration = 300;
 
-$(".picture").click(function() {
-    $("#overlay").show();
-    $(this).addClass("pic-click");
-    document.body.style.overflow="hidden";
-    $(".back-to-top").hide();
-  });
-  
-  $("#overlay").click(function() {
-    $(this).hide();
-    $(".picture").removeClass("pic-click");
-    document.body.style.overflow="visible";
-    $(".back-to-top").show();
-  });
-
-$(".item").click(function() {
-  if ($(this).hasClass("clicked")) {
-  } else {
-    $(".item").removeClass("clicked");
-    $(this).addClass("clicked");
-  }
-  // console.log($(this).children("span").attr("id") == "places-btn");
-  // divSwitch();
-  var isPeople = $(this).children("span").attr("id") == "people-btn"
-  var isPlaces = $(this).children("span").attr("id") == "places-btn"
-  var isFood =$(this).children("span").attr("id") == "food-btn"
-
-
-  if (isPeople) {
-        $('.thumbnail-wrapper').css("display", "none");
-        $('#people-content').css("display", "block");
-    } else if (isPlaces) {
-        $('.thumbnail-wrapper').css("display", "none");
-        $('#places-content').css("display", "block");
-    } else {
-        $('.thumbnail-wrapper').css("display", "none");
-        $('#food-content').css("display", "block");
-    }
-});
- 
-
-});
-
-function cycleImages(){
-    $("#cycler").each(function(){
-      var $active = $(this).find(".active");
-      var $next = ($(this).find(".active").next().length > 0) ? $(this).find(".active").next() : $(this).find("img:first");
-      $next.css("z-index",2);//move the next image up the pile
-      $active.fadeOut(1500,function(){//fade out the top image
-      $active.css("z-index",1).show().removeClass("active");//reset the z-index and unhide the image
-      $next.css("z-index",3).addClass("active");//make the next image the top one
+	// lazy loading
+    $(".lazy").Lazy({
+    	scrollDirection: "vertical",
+    	effect: "fadeIn",
+    	visibleOnly: true
     });
-  });
+    // lazy loading
+ 
+	$(window).scroll(function() {
+		if ($(this).scrollTop() > offset) {
+			$(".back-to-top").fadeIn(duration);
+		} else {
+			$(".back-to-top").fadeOut(duration);
+		}
+	});
+	 
+	$(".back-to-top").click(function(event) {
+		event.preventDefault();
+		$("html, body").animate({scrollTop: 0}, duration);  
+	return false;
+	});
+
+	$(".picture").click(function() {
+			$("#overlay").show();
+			$(this).addClass("pic-click");
+			document.body.style.overflow="hidden";
+			$(".back-to-top").hide();
+		});
+		
+		$("#overlay").click(function() {
+			$(this).hide();
+			$(".picture").removeClass("pic-click");
+			document.body.style.overflow="visible";
+			$(".back-to-top").show();
+		});
+
+	$(".item").click(function() {
+		if ($(this).hasClass("clicked")) {
+		} else {
+			$(".item").removeClass("clicked");
+			$(this).addClass("clicked");
+		}
+		// console.log($(this).children("span").attr("id") == "places-btn");
+		// divSwitch();
+		var isPeople = $(this).children("span").attr("id") == "people-btn"
+		var isPlaces = $(this).children("span").attr("id") == "places-btn"
+		var isFood =$(this).children("span").attr("id") == "food-btn"
+
+
+	if (isPeople) {
+				$('.thumbnail-wrapper').css("display", "none");
+				$('#people-content').css("display", "block");
+		} else if (isPlaces) {
+				$('.thumbnail-wrapper').css("display", "none");
+				$('#places-content').css("display", "block");
+		} else {
+				$('.thumbnail-wrapper').css("display", "none");
+				$('#food-content').css("display", "block");
+		}
+	});
+});
+
+function cycleImages() {
+	$("#cycler").each(function() {
+		var $active = $(this).find(".active");
+		var $next = ($(this).find(".active").next().length > 0) ? $(this).find(".active").next() : $(this).find("img:first");
+		$next.css("z-index",2);//move the next image up the pile
+		$active.fadeOut(1500,function() {//fade out the top image
+			$active.css("z-index",1).show().removeClass("active");//reset the z-index and unhide the image
+			$next.css("z-index",3).addClass("active");//make the next image the top one
+		});
+	});
 }
 
 
@@ -83,7 +88,7 @@ setInterval("cycleImages()", 5000);
 // $(document).ready(function() {
 
 //   //Implement the showing and hiding of the sidebar when the user clicks on #sidebar-button here:
-  
+	
 
 
 //   // $("#logo").hover(function(){
@@ -102,7 +107,7 @@ setInterval("cycleImages()", 5000);
 //         $("#bone").removeClass("boneani");
 //         // $("#paw").removeClass("pawani");
 //     });
-  
+	
 
 //   $("#sidebar-button").click(function() {
 //     if ($(".sidebar-container").hasClass("sidebar-active")) {
@@ -192,7 +197,7 @@ setInterval("cycleImages()", 5000);
 //     $("#modal-container").hide();
 //     $("body").removeClass("no-scroll");
 //   });
-  
+	
 //   // IMPLEMENT "HIDE MODAL" WHEN "CLICK ON MODAL OVERLAY" HERE
 //   $("#modal-overlay").click(function() {
 //     $("#modal-container").hide();
